6d5a4f3dd4a0931794e8b221e2c6716478c871e0
Bram Oosterhuis <mail@bybram.com>
2021-06-02 10:25:11 +0200

    make sure script is sourced

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
-#-DCMAKE_TOOLCHAIN_FILE="/media/bram/Projects/build-env/buildroot-rpi/output/host/usr/share/buildroot/toolchainfile.cmake"
-export THUNDER_PROJECT_ROOT_LOCATION="${PWD}"
-export THUNDER_BUILD_TYPE="Debug"
-export THUNDER_TOOLS_LOCATION="${THUNDER_PROJECT_ROOT_LOCATION}/host-tools"
 
-export LD_LIBRARY_PATH="${THUNDER_PROJECT_ROOT_LOCATION}/install/usr/lib:${LD_LIBRARY_PATH}"
-export PATH="${THUNDER_PROJECT_ROOT_LOCATION}/install/usr/bin;${THUNDER_PROJECT_ROOT_LOCATION}/install/usr/sbin:${PATH}"
+function prepare() {
+    #-DCMAKE_TOOLCHAIN_FILE="/media/bram/Projects/build-env/buildroot-rpi/output/host/usr/share/buildroot/toolchainfile.cmake"
+    export THUNDER_PROJECT_ROOT_LOCATION="${PWD}"
+    export THUNDER_BUILD_TYPE="Debug"
+    export THUNDER_TOOLS_LOCATION="${THUNDER_PROJECT_ROOT_LOCATION}/host-tools"
 
-# install host tooling
-cmake -Hsource/Tools -Bbuild/ThunderHostTools  -DCMAKE_MODULE_PATH=${THUNDER_TOOLS_LOCATION} -DCMAKE_INSTALL_PREFIX=${THUNDER_TOOLS_LOCATION} -DGENERIC_CMAKE_MODULE_PATH=${THUNDER_TOOLS_LOCATION}
-cmake --build build/ThunderHostTools --target install
+    export LD_LIBRARY_PATH="${THUNDER_PROJECT_ROOT_LOCATION}/install/usr/lib:${LD_LIBRARY_PATH}"
+    export PATH="${THUNDER_PROJECT_ROOT_LOCATION}/install/usr/bin;${THUNDER_PROJECT_ROOT_LOCATION}/install/usr/sbin:${PATH}"
 
-echo "Build environment:"
-echo " - Type:                 ${THUNDER_BUILD_TYPE}"
-echo " - Project root:         ${THUNDER_PROJECT_ROOT_LOCATION}"
-echo " - Host tools:           ${THUNDER_TOOLS_LOCATION}"
+    # install host tooling
+    cmake -Hsource/Tools -Bbuild/ThunderHostTools  -DCMAKE_MODULE_PATH=${THUNDER_TOOLS_LOCATION} -DCMAKE_INSTALL_PREFIX=${THUNDER_TOOLS_LOCATION} -DGENERIC_CMAKE_MODULE_PATH=${THUNDER_TOOLS_LOCATION}
+    cmake --build build/ThunderHostTools --target install
+
+    echo "Build environment:"
+    echo " - Type:                 ${THUNDER_BUILD_TYPE}"
+    echo " - Project root:         ${THUNDER_PROJECT_ROOT_LOCATION}"
+    echo " - Host tools:           ${THUNDER_TOOLS_LOCATION}"
+}
+
+[[ $_ != $0 ]] && prepare || echo "OOOOOPS... You're running this script, but it should be 'sourced'. Exiting now."
