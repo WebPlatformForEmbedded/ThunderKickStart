9230fbec9c01981d4c98acb4525332fa837fbdad
Bram Oosterhuis <mail@bybram.com>
2021-06-02 10:25:39 +0200

    fix PATH variable

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -7,7 +7,7 @@ function prepare() {
     export THUNDER_TOOLS_LOCATION="${THUNDER_PROJECT_ROOT_LOCATION}/host-tools"
 
     export LD_LIBRARY_PATH="${THUNDER_PROJECT_ROOT_LOCATION}/install/usr/lib:${LD_LIBRARY_PATH}"
-    export PATH="${THUNDER_PROJECT_ROOT_LOCATION}/install/usr/bin;${THUNDER_PROJECT_ROOT_LOCATION}/install/usr/sbin:${PATH}"
+    export PATH="${THUNDER_PROJECT_ROOT_LOCATION}/install/usr/bin:${THUNDER_PROJECT_ROOT_LOCATION}/install/usr/sbin:${PATH}"
 
     # install host tooling
     cmake -Hsource/Tools -Bbuild/ThunderHostTools  -DCMAKE_MODULE_PATH=${THUNDER_TOOLS_LOCATION} -DCMAKE_INSTALL_PREFIX=${THUNDER_TOOLS_LOCATION} -DGENERIC_CMAKE_MODULE_PATH=${THUNDER_TOOLS_LOCATION}
