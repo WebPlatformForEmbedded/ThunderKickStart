552e563bf5c5428bfffc53c3889a2f7add85d42b
Bram Oosterhuis <mail@bybram.com>
2021-07-21 14:43:28 +0200

    initial remote debug support

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# set -x
 
 function print_usage() {
     echo "Usage:"
@@ -62,10 +63,20 @@ function parse_args(){
 function cmake-install(){
     if [[ -f $TOOLCHAIN_FILE ]]
     then
-       THUNDER_TOOLCHAIN_FILE=-DCMAKE_TOOLCHAIN_FILE="${TOOLCHAIN_FILE}"
+       THUNDER_TOOLCHAIN=-DCMAKE_TOOLCHAIN_FILE="${TOOLCHAIN_FILE}"    
+    elif [[ -f "${BUILD_TOOLS_LOCATION}${BUILD_TOOLS_PREFIX}g++" ]] && [[ -f "${BUILD_TOOLS_LOCATION}${BUILD_TOOLS_PREFIX}gcc" ]]
+    then
+        THUNDER_TOOLCHAIN=' -DCMAKE_C_COMPILER="${BUILD_TOOLS_LOCATION}${BUILD_TOOLS_PREFIX}gcc"'
+        THUNDER_TOOLCHAIN+=' -DCMAKE_CXX_COMPILER="${BUILD_TOOLS_LOCATION}${BUILD_TOOLS_PREFIX}g++"'
+        THUNDER_TOOLCHAIN+=' -DCMAKE_PROGRAM_PATH="${BUILD_TOOLS_LOCATION}"'
+        if [[ "x$SYS_ROOT" != "x" ]]
+        then
+            THUNDER_TOOLCHAIN+=' -DCMAKE_SYSROOT="${SYS_ROOT}"'
+            THUNDER_TOOLCHAIN+=' -DCMAKE_FIND_ROOT_PATH="${SYS_ROOT}"'
+        fi
     fi
    
-    cmake -Hsource/$1 -Bbuild/${1///} $THUNDER_TOOLCHAIN_FILE -DCMAKE_MODULE_PATH=${TOOLS_LOCATION} -DCMAKE_INSTALL_PREFIX=${TOOLS_LOCATION} -DGENERIC_CMAKE_MODULE_PATH=${TOOLS_LOCATION} &> $STDOUT
+    cmake -Hsource/$1 -Bbuild/${1///} $THUNDER_TOOLCHAIN -DCMAKE_MODULE_PATH=${TOOLS_LOCATION} -DCMAKE_INSTALL_PREFIX=${TOOLS_LOCATION} -DGENERIC_CMAKE_MODULE_PATH=${TOOLS_LOCATION} &> $STDOUT
     cmake --build build/${1///} --target install &> $STDOUT
 }
 
@@ -86,58 +97,151 @@ function pre-install(){
 }
 
 function check_env(){
+    local DEFAULT=""
+
     if [ "x$BUILD_TYPE" = "x" ]
     then
-    	BUILD_TYPE="Debug"
-        read -e -p "Set Build type: " -i "$BUILD_TYPE" BUILD_TYPE
+    	DEFAULT="Debug"
+        read -e -p "Set Build type [$DEFAULT]: " USER_INPUT
+        BUILD_TYPE="${USER_INPUT:-$DEFAULT}"
     fi
     
     if [ "x$ROOT_LOCATION" = "x" ]
     then
-    	ROOT_LOCATION="${PWD}"
-        read -e -p "Set root install location: " -i "$ROOT_LOCATION" ROOT_LOCATION
+    	DEFAULT="${PWD}"
+        read -e -p "Project root location [$DEFAULT]: " USER_INPUT
+        ROOT_LOCATION="${USER_INPUT:-$DEFAULT}"
     fi
     
     if [ "x$INSTALL_LOCATION" = "x" ]
     then
-    	INSTALL_LOCATION="${ROOT_LOCATION}/install"
-        read -e -p "Set thunder install location: " -i "$INSTALL_LOCATION" INSTALL_LOCATION
+    	DEFAULT="${ROOT_LOCATION}/install"
+        read -e -p "Thunder install location [$DEFAULT]: "  USER_INPUT
+        INSTALL_LOCATION="${USER_INPUT:-$DEFAULT}"
     fi
     
     if [ "x$TOOLS_LOCATION" = "x" ]
     then
-    	TOOLS_LOCATION="${ROOT_LOCATION}/host-tools"
-        read -e -p "Set tools install location: " -i "$TOOLS_LOCATION" TOOLS_LOCATION
+    	DEFAULT=${ROOT_LOCATION}/host-tools
+        read -e -p "Thunder host tools install location[$DEFAULT]: " USER_INPUT
+        TOOLS_LOCATION="${USER_INPUT:-$DEFAULT}"
     fi
-  
+
     if [ "x$TOOLCHAIN_FILE" = "x" ]
     then
-        read -e -p "Use Toolchain File: " TOOLCHAIN_FILE
-        TOOLCHAIN_FILE=${TOOLCHAIN_FILE:-N}
+        DEFAULT="N"
+        read -e -p "Use Toolchain File?: " USER_INPUT
+        TOOLCHAIN_FILE="${USER_INPUT:-$DEFAULT}"
+    fi
+
+    if ! [[ -f TOOLCHAIN_FILE ]]
+    then
+        echo "No toolchain file."
+        if [ "x$BUILD_TOOLS_LOCATION" = "x" ]
+        then
+            DEFAULT="/usr/bin/"
+            read -e -p "toolchain build tools location [$DEFAULT]:" -i "$BUILD_TOOLS_LOCATION" USER_INPUT
+            BUILD_TOOLS_LOCATION="${USER_INPUT:-$DEFAULT}"
+        fi
+
+        if [ "x$BUILD_TOOLS_PREFIX" = "x" ]
+        then
+            DEFAULT=""
+            read -e -p "build tools prefix [$DEFAULT]: " -i "$BUILD_TOOLS_PREFIX" USER_INPUT
+            BUILD_TOOLS_PREFIX="${USER_INPUT:-$DEFAULT}"
+        fi
+    fi
+
+    check_tools
+
+}
+
+function sanitize_dir {
+    if [[ -d $1 ]]
+    then
+        local dir=$(realpath "$1")
+        dir+=/
+        echo $dir
     fi
 }
 
+function check_tools {
+    local EXPECTED_TOOLS=(
+    	gcc 
+    	g++
+    	gdb
+    )
+
+    for TOOL in "${EXPECTED_TOOLS[@]}"
+    do
+        VERSION=$(${BUILD_TOOLS_LOCATION}${BUILD_TOOLS_PREFIX}${TOOL} --version)
+
+        if [[ "x${VERSION}" = "x" ]]
+        then
+            echo ""
+            echo "ERROR: Missing $TOOL."
+            echo ""
+            false
+        else
+            echo ""
+            echo "Found ${BUILD_TOOLS_LOCATION}${BUILD_TOOLS_PREFIX}${TOOL}."
+            echo "${VERSION}."
+            echo ""
+        fi
+    done
+set -x
+    SYS_ROOT=$(${BUILD_TOOLS_LOCATION}${BUILD_TOOLS_PREFIX}gcc -print-sysroot)
+
+    set +x
+
+    IFS='-' read -r TARGET_ARCH LEFTOVER <<< $(${BUILD_TOOLS_LOCATION}${BUILD_TOOLS_PREFIX}gcc -dumpmachine)
+    echo "TARGET_ARCH ${TARGET_ARCH}"
+}
+
 function write_workspace(){
+    THUNDER_TOOLCHAIN=""
+    THUNDER_INSTALL_TARGET_PREFIX=""
+
+    set -x
+    
+    if [[ -f $TOOLCHAIN_FILE ]]
+    then
+        THUNDER_TOOLCHAIN="\"CMAKE_TOOLCHAIN_FILE\":\"${TOOLCHAIN_FILE}\","
+    elif [[ -f "${BUILD_TOOLS_LOCATION}${BUILD_TOOLS_PREFIX}g++" ]] && 
+         [[ -f "${BUILD_TOOLS_LOCATION}${BUILD_TOOLS_PREFIX}gcc" ]]
+    then
+        THUNDER_TOOLCHAIN="\"CMAKE_C_COMPILER\":\"${BUILD_TOOLS_LOCATION}${BUILD_TOOLS_PREFIX}gcc\","
+        THUNDER_TOOLCHAIN+="\"CMAKE_CXX_COMPILER\":\"${BUILD_TOOLS_LOCATION}${BUILD_TOOLS_PREFIX}g++\","
+        THUNDER_TOOLCHAIN+="\"CMAKE_PROGRAM_PATH\": \"${BUILD_TOOLS_LOCATION}\","
+        if [[ "x$SYS_ROOT" != "x" ]]
+        then
+            THUNDER_TOOLCHAIN+="\"CMAKE_SYSROOT\": \"${SYS_ROOT}\","
+            THUNDER_TOOLCHAIN+="\"CMAKE_FIND_ROOT_PATH\": \"${SYS_ROOT}\","
+        else
+            THUNDER_INSTALL_TARGET_PREFIX="$INSTALL_LOCATION"
+        fi
+    fi
+
+    set +x
+
     write_vscode_workspace
 }
 
 function write_vscode_workspace(){
-    if [[ -f $TOOLCHAIN_FILE ]]
-    then
-        THUNDER_TOOLCHAIN_FILE="\"CMAKE_TOOLCHAIN_FILE\":\"${TOOLCHAIN_FILE}\","
-    fi
-    
-    timestamp=`date +'%s'`
-    
-    tmpws="${timestamp}-Thunder.code-workspace"
-    ws=${USER}-Thunder.code-workspace
+    local timestamp=`date +'%s'`
+    local tmpws="${timestamp}-Thunder.code-workspace"
+    local ws=${USER}-Thunder.code-workspace
     
     sed \
-        -e "s|@THUNDER_TOOLCHAIN_FILE@|${THUNDER_TOOLCHAIN_FILE}|g" \
+        -e "s|@THUNDER_TOOLCHAIN@|${THUNDER_TOOLCHAIN}|g" \
         -e "s|@THUNDER_INSTALL_LOCATION@|${INSTALL_LOCATION}|g" \
+        -e "s|@THUNDER_INSTALL_TARGET_PREFIX@|${THUNDER_INSTALL_TARGET_PREFIX}|g" \
         -e "s|@THUNDER_TOOLS_LOCATION@|${TOOLS_LOCATION}|g" \
         -e "s|@THUNDER_BUILD_TYPE@|${BUILD_TYPE}|g" \
         -e "s|@THUNDER_PROJECT_ROOT_LOCATION@|${ROOT_LOCATION}|g" \
+        -e "s|@BUILD_TOOLS_PREFIX@|${BUILD_TOOLS_PREFIX}|g" \
+        -e "s|@BUILD_TOOLS_LOCATION@|${BUILD_TOOLS_LOCATION}|g" \
+        -e "s|@SYS_ROOT@|${SYS_ROOT}|g" \
         source/workspaces/Thunder.code-workspace > ${tmpws}
 
     if [[ -f ${ws} ]]
@@ -177,7 +281,9 @@ function write_cache() {
     echo "INSTALL_LOCATION=\"$INSTALL_LOCATION\"" >> .cache
     echo "TOOLS_LOCATION=\"$TOOLS_LOCATION\"" >> .cache
     echo "TOOLCHAIN_FILE=\"$TOOLCHAIN_FILE\"" >> .cache
-    
+    echo "BUILD_TOOLS_PREFIX=\"$BUILD_TOOLS_PREFIX\"" >> .cache
+    echo "BUILD_TOOLS_LOCATION=\"$BUILD_TOOLS_LOCATION\"" >> .cache
+
     show_env
 }
 
@@ -186,10 +292,15 @@ function show_env(){
     echo " - Type:                  ${BUILD_TYPE}"
     echo " - Project root location: ${ROOT_LOCATION}"
     echo " - Install location       ${INSTALL_LOCATION}"
-    echo " - Host tools:            ${TOOLS_LOCATION}"
+    echo " - Thunder tools:         ${TOOLS_LOCATION}"
     if [[ -f $TOOLCHAIN_FILE ]]
     then
     echo " - CMake toolchain file:  ${TOOLCHAIN_FILE}"
+    fi
+    if [[ -d $BUILD_TOOLS_LOCATION ]]
+    then
+    echo " - Toolchain location:    ${BUILD_TOOLS_LOCATION}"
+    echo " - Toolchain prefix:      ${BUILD_TOOLS_PREFIX}"
     fi
 } 
 
@@ -203,11 +314,14 @@ function main() {
     STDOUT="/dev/null"
     
     # Initialize variables
+    BUILD_TOOLS_LOCATION=""
+    BUILD_TOOLS_PREFIX=""
     BUILD_TYPE=""
     ROOT_LOCATION=""
     INSTALL_LOCATION=""
     TOOLS_LOCATION=""
     TOOLCHAIN_FILE=""
+    SYS_ROOT=""
     
     # only if the path exists in sources dir
     PREINSTALL_COMPONENTS=(
